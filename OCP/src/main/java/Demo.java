0cf6246d4c0703666449e600e03a94084302739f
padamv <padamv91@gmail.com>
2021-01-24 11:42:20 +0100

    Create main for the project

---
+++ OCP/src/main/java/Demo.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        Product apple = new Product("Apple", Color.GREEN, Size.SMALL);
+        Product tree = new Product("Tree", Color.GREEN, Size.LARGE);
+        Product house = new Product("House", Color.BLUE, Size.LARGE);
+
+        List<Product> products = List.of(apple, tree, house);
+
+        ProductFilter productFilter = new ProductFilter();
+        System.out.println("Green products (old): ");
+        productFilter.filterByColor(products, Color.GREEN)
+                .forEach(product -> System.out.println(
+                        " - " + product.getName() + " is green"));
+    }
+}
--- OCP/src/main/java/ProductFilter.java
+++ OCP/src/main/java/ProductFilter.java
@@ -4,11 +4,11 @@ import java.util.stream.Stream;
 
 
 public class ProductFilter {
-    private Stream<Product> filterByColor(List<Product> products, Color color){
+    public Stream<Product> filterByColor(List<Product> products, Color color){
         return products.stream().filter(p -> p.getColor() == color);
     }
 
-    private Stream<Product> filterBySize(List<Product> products, Size size){
+    public Stream<Product> filterBySize(List<Product> products, Size size){
         return products.stream().filter(p -> p.getSize() == size);
     }
 
