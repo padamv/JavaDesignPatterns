bb7a8ad85b4fe5f3911fe23c7fe4bf244ff077dc
padamv <padamv91@gmail.com>
2021-01-25 10:44:00 +0100

    Demonstrate EmployeeBuilder

--- BuilderExample/src/main/java/App/Demo.java
+++ BuilderExample/src/main/java/App/Demo.java
@@ -1,6 +1,9 @@
 package App;
 
+import Service.EmployeeBuilder;
 import Service.HtmlBuilder;
+
+import java.sql.SQLOutput;
 
 public class Demo {
     public static void main(String[] args) {
@@ -8,7 +11,11 @@ public class Demo {
         builder
                 .addChild("li", "hello")
                 .addChild("li", "world");
-        System.out.println(builder);
+       // System.out.println(builder);
 
+        EmployeeBuilder employeeBuilder = new EmployeeBuilder()
+                .withName("Adam")
+                .worksAs("Developer");
+        System.out.println(employeeBuilder.build());
     }
 }
