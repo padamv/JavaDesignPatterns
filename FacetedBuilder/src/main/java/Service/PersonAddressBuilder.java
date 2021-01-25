2ca82f18e0d09a766ffb6193c09b4ea24c9b9cae
padamv <padamv91@gmail.com>
2021-01-25 11:04:07 +0100

    Create Address builder for person

---
+++ FacetedBuilder/src/main/java/Service/PersonAddressBuilder.java
@@ -0,0 +1,25 @@
+package Service;
+
+import Entities.Person;
+
+public class PersonAddressBuilder extends PersonBuilder {
+
+    public PersonAddressBuilder(Person person){
+        this.person = person;
+    }
+
+    public PersonAddressBuilder at (String streetAddress){
+        person.setStreetAddress(streetAddress);
+        return this;
+    }
+
+    public PersonAddressBuilder withPostcode (String postcode){
+        person.setPostcode(postcode);
+        return this;
+    }
+
+    public PersonAddressBuilder in (String city){
+        person.setCity(city);
+        return this;
+    }
+}
