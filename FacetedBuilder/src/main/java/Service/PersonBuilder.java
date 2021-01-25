b0db5a053dabcb605b99e508a48de5b50f868f9a
padamv <padamv91@gmail.com>
2021-01-25 11:06:40 +0100

    Add personAddressBuilder to PersonBuilder

--- FacetedBuilder/src/main/java/Service/PersonBuilder.java
+++ FacetedBuilder/src/main/java/Service/PersonBuilder.java
@@ -6,6 +6,10 @@ public class PersonBuilder {
 
     protected Person person = new Person();
 
+    public PersonAddressBuilder lives(){
+        return new PersonAddressBuilder(person);
+    }
+
     public Person build(){
         return person;
     }
