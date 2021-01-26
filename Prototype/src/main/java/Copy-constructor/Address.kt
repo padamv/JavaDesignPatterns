39581f840e3324d9e20898f9c74607e793b0f2f9
padamv <padamv91@gmail.com>
2021-01-26 16:45:01 +0100

    Create Address class with copy constructor

--- Prototype/src/main/java/Copy-constructor/Address.kt
+++
@@ -1,4 +0,0 @@
-package `Copy-constructor`
-
-class Address {
-}
---
+++ Prototype/src/main/java/copyConstructor/Address.java
@@ -0,0 +1,24 @@
+package copyConstructor;
+
+public class Address {
+    private String streetAddress, city, country;
+
+    public Address(String streetAddress, String city, String country){
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.country = country;
+    }
+
+    public Address (Address other){
+        this(other.streetAddress, other.city, other.country);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
