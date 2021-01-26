19cc1acb345ce9aee08597fe52c502858658bf2e
padamv <padamv91@gmail.com>
2021-01-26 14:46:29 +0100

    Create constructor with coordinate system handling

--- FactoryMethod/src/main/java/Entities/Point.java
+++ FactoryMethod/src/main/java/Entities/Point.java
@@ -1,4 +1,20 @@
 package Entities;
 
 public class Point {
+
+    private double x, y;
+
+    public Point(double a, double b, CoordinateSystem coordinateSystem) {
+        switch (coordinateSystem) {
+            case CARTESIAN -> {
+                this.x = a;
+                this.y = b;
+            }
+            case POLAR -> {
+                this.x = a * Math.cos(b);
+                this.y = a * Math.sin(b);
+            }
+        }
+
+    }
 }
