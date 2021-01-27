734e6821370b49bf575b3eef3debeac05118c9dc
padamv <padamv91@gmail.com>
2021-01-27 09:46:50 +0100

    Create Line

---
+++ Adapter/src/main/java/Entities/Line.java
@@ -0,0 +1,26 @@
+package Entities;
+
+public class Line {
+    private Point start, end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Point getStart() {
+        return start;
+    }
+
+    public void setStart(Point start) {
+        this.start = start;
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
+    public void setEnd(Point end) {
+        this.end = end;
+    }
+}
